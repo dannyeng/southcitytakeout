b4e2a03436b28b86d7934a206acbaa72a9902d0f
Clement Hoang <clement.hoang24@gmail.com>
2015-08-19 12:26:44 -0400

    Links “Order Now” button to Contact anchor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,30 @@
 (function($) {
 	$( document ).ready(function() {
-		
+
 		//Init Nice Scroll
 		$("html").niceScroll({'scrollspeed':50});
-		
+
 		// Init WOW Effects
 		new WOW({ mobile:false }).init();
-		
-		
-		// Book a Table Button
-		$('.btn-navbar-book').click(function(e) {
-			e.preventDefault();
-			$('html, body').stop().animate({
-				scrollTop: $('#Book').offset().top
-			}, 1500,'easeInOutExpo');
-		});
-		
-		
+
 		// Food Categories Menu
 		$('.categories a').click(function(e) {
 			e.preventDefault();
-			
+
 			$('.categories a').removeClass('active');
 			$(this).addClass('active');
-			
+
 			$('.category-content').stop().clearQueue().slideUp();
 			$($(this).attr('href')).slideDown();
 		});
-		
-		
-		
-		
-		
+
+
+
+
+
 			/* Booking Table Form Submission */
 			var use_ajax = true;
-		
+
 			$('#BookTable').submit(function() {
 				var form_data = $(this).serialize();
 				if (use_ajax === true)
@@ -45,20 +35,20 @@
 					return false;
 				}
 			});
-		
-		
-		
+
+
+
 		// Gallery Owl Carousel Init
 		$('.owl-testimonials').owlCarousel({
 				loop:true,
-				margin:10,				
+				margin:10,
 				nav:false,
 				items:1,
 				autoplay:true,
 				responsiveClass:true
 		})
-		
-		
+
+
 		// Gallery Owl Carousel Init
 		$('.owl-carousel').owlCarousel({
 				loop:true,
@@ -80,23 +70,23 @@
 						}
 				}
 		})
-		
-		
-		
-		// Social Media Icons		
+
+
+
+		// Social Media Icons
 	 	$(".footer-social li").mouseleave(function(){
         $(this).find('img').stop().css('top', '0');
     }).mouseenter(function(){
         $(this).find('img').stop().animate({ top: '-10px' }, 250);
     });
-		
-		
+
+
 			/* Contact Form Submission */
 			var use_ajax = true;
-		
+
 			$('#ContactForm').submit(function() {
 				var form_data = $(this).serialize();
-				
+
 				if (validate_email($('#ContactEmail').val() ))
 				{
 					if (use_ajax === true)
@@ -113,24 +103,24 @@
 					return false;
 				}
 			});
-	
-	
-	
+
+
+
 		/*-----------------------------------------------------------------------------------*/
 		/* ####	HELPERS
 		/*-----------------------------------------------------------------------------------*/
-	
-		
+
+
 		/* Validate E-Mail */
-	
-		function validate_email (email) { 
+
+		function validate_email (email) {
 			// http://stackoverflow.com/a/46181/11236
-			
+
 				var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 				return re.test(email);
 		}
-	
-		
+
+
 		// Smooth scroll
 		$('a.scroll').bind('click',function(event){
 			var $anchor = $(this);
@@ -140,8 +130,8 @@
 			event.preventDefault();
 			return false;
 		});
-		
 
-	
-	});	
+
+
+	});
 })(jQuery);
